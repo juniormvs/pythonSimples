10910acf4939cfcdbe1f0c714421c2963a98529a
juniormvs <juniormvs@hotmail.com>
2025-12-02 19:41:09 -0300

    finalização do teste

--- exemplo.py
+++ exemplo.py
@@ -2,7 +2,8 @@ print("Hello world")
 idade=38
 nome = input("Digite seu nome ")
 print(nome, idade)
-python_esta_legal = True
+python_esta_legal = "Verdadeiro " \
+"Sim, Python é legal"
 print(python_esta_legal)
 
 print("Fim do programa")
